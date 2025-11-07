c513f83b0d8cd20555927930bc85af8fb149c732
VelpaChallenger <velpachallenger@gmail.com>
2025-11-06 23:04:35 -0300

    fix: error if enter on CHR bank empty
    Come on, don't do that! Fine! Fixed :)

--- EntryReturn.py
+++ EntryReturn.py
@@ -5,6 +5,10 @@ class EntryReturn:
 
     def chr_entry(self, event=None): #chr is reserved keyword and should not matter in this context but I still feel more comfortable, if not technically then aesthetically when I see chr_entry.
         chr_entry_value = self.createanims.chr_entry.get()
+        if not chr_entry_value:
+            self.createanims.chr_entry.configure(highlightcolor="red", highlightbackground="red")
+            self.createanims.chr_info_text.configure(text="You haven't entered a CHR Bank number yet.", fg="red")
+            return False
         if int(chr_entry_value) % 2 == 1: #Validation 4: number must be even. Though for this, it will have to be on enter, there's no other way to know the input is finished.
             self.createanims.chr_entry.configure(highlightcolor="red", highlightbackground="red")
             self.createanims.chr_info_text.configure(text="CHR Bank must be an even number.", fg="red")
