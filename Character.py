4f1c35d3941564bf75423958d262342f965cb870
VelpaChallenger <velpachallenger@gmail.com>
2025-11-07 00:55:33 -0300

    feat: can load entire character data on init
    Awesome!

--- Character.py
+++ Character.py
@@ -1,3 +1,5 @@
+import os
+
 from Anim import *
 
 class Character:
@@ -6,10 +8,15 @@ class Character:
         self.name = character_name #Not sure if we'll need it but... maybe for display purposes? We could add a dictionary or something... anyways, don't want to include too much in this final commit before going back to main.
         self.createanims = createanims
         self.palette = self.get_palette(character_name)
-        self.anims = self.get_anims(character_name, [0x00])
-        self.frames = self.get_frames(character_name, [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07])
-        chr_ids = [self.frames[1].metadata.chr_bank, 0x9E] #When I add more characters, there'll be more and they'll be obtained after we parse the anim file.
+        self.anims = self.get_anims(character_name, self.get_anims_ids(character_name))
+        self.frames = self.get_frames(character_name, self.get_frames_ids(character_name))
+        chr_ids = self.get_chrs_ids(character_name) #When I add more characters, there'll be more and they'll be obtained after we parse the anim file.
         self.chr_palettes, self.chrs = self.get_chrs_and_palettes(character_name, chr_ids) #Whatever, the and makes it clear. Any other name to include both just doesn't make it clear.
+
+    def get_chrs_ids(self, name):
+        chrs_filenames=os.listdir(f"{self.createanims.root_dir}{os.sep}{name}{os.sep}chr") #Don't ask me why here I use os.sep and in other places I use forward slash.
+        chrs_ids = [int(chr_filename.split(".chr.pal")[0][-3:]) for chr_filename in chrs_filenames if chr_filename[-4:] == ".pal"] #Let's use pal to get the IDs and avoid duplicates.
+        return chrs_ids
 
     def get_chrs_and_palettes(self, name, chr_ids): #Yeah, after all we're kinda going to do the loop thing now, though not entirely but leaving the bases.
         chr_palettes = {}
@@ -36,6 +43,11 @@ class Character:
             character_chr = list(character_chr.read())
         return character_chr #Added character cause otherwise chr, you know, reserved keyword.
 
+    def get_frames_ids(self, name):
+        frames_filenames=os.listdir(f"{self.createanims.root_dir}{os.sep}{name}{os.sep}frames") #Don't ask me why here I use os.sep and in other places I use forward slash.
+        frames_ids = [int(frame_filename.split(".frame")[0][-2:]) for frame_filename in frames_filenames]
+        return frames_ids
+
     def get_frames(self, name, frame_ids):
         frames = [] #This one will be 0-indexed. Frame 00, frame 01 etc.
         for frame_id in frame_ids: #Potentially range(frame_ids) or something of the sort.
@@ -47,6 +59,11 @@ class Character:
         with open(f"{self.createanims.root_dir}/{name}/frames/{name}_frame_{frame_id:02d}.frame", "rb") as character_frame:
             frame = Frame(list(character_frame.read()))
         return frame
+
+    def get_anims_ids(self, name):
+        anims_filenames=os.listdir(f"{self.createanims.root_dir}{os.sep}{name}{os.sep}anims") #Don't ask me why here I use os.sep and in other places I use forward slash.
+        anims_ids = [int(anim_filename.split(".anim")[0][-2:]) for anim_filename in anims_filenames]
+        return anims_ids
 
     def get_anims(self, name, anim_ids):
         anims = [] #This one will be 0-indexed too. Anim 00, Anim 01 etc. #Might change this soon enough because some anims don't have files, like I removed them, like anim 0xF I believe. Yeah just checked.
